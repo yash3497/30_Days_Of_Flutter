ebf966abd242b7c47f0cc4d0a33e916d6efcdb62
yash gupta <yash.gupta8791133497@gmail.com>
2022-01-18 14:42:46 +0530

    Theme extractor complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
 import 'package:flutter_catalog/utils/routes.dart';
+import 'package:flutter_catalog/widgets/theme.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'pages/home_page.dart';
@@ -15,13 +16,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: ThemeMode.light,
-      theme: ThemeData(
-          primarySwatch: Colors.blue,
-          fontFamily: GoogleFonts.lato().fontFamily),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
+      themeMode: ThemeMode.dark,
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       routes: {
         "/": (context) => HomePage(),
         MyRoutes.homeRoute: (context) => HomePage(),
---
+++ lib/widgets/theme.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyTheme {
+  static ThemeData lightTheme(BuildContext context) => ThemeData(
+      primarySwatch: Colors.blue,
+      fontFamily: GoogleFonts.lato().fontFamily,
+      appBarTheme: AppBarTheme(
+        color: Colors.white,
+        elevation: 0.0,
+        iconTheme: IconThemeData(color: Colors.black),
+        toolbarTextStyle: Theme.of(context).textTheme.bodyText2,
+        titleTextStyle: Theme.of(context).textTheme.headline6,
+      ));
+
+  static ThemeData darkTheme(BuildContext context) => ThemeData(
+      brightness: Brightness.dark,
+      fontFamily: GoogleFonts.lato().fontFamily,
+      appBarTheme: AppBarTheme(
+        // color: Colors.black,
+        elevation: 0.0,
+        // iconTheme: IconThemeData(color: Colors.white),
+        // toolbarTextStyle: Theme.of(context).textTheme.bodyText2,
+        // titleTextStyle: Theme.of(context).textTheme.headline6,
+      ));
+}
