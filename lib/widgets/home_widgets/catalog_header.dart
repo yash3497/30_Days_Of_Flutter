ed5f7211eb23a6a05b133cb123474d44452b1f1a
yash gupta <yash.gupta8791133497@gmail.com>
2022-01-20 12:02:26 +0530

    Hero animation

---
+++ lib/widgets/home_widgets/catalog_header.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_catalog/widgets/theme.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class catalogHeader extends StatelessWidget {
+  const catalogHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        "Catalog APP".text.xl5.bold.color(MyTheme.darkBluishColor).make(),
+        "Trending Products".text.xl2.make()
+      ],
+    );
+  }
+}
